753d813ccc3fd5d41a9898dbc35ff74145a943fe
Cliff L. Biffle <code@cliffle.com>
2024-10-16 22:38:09 -0700

    Add license to prepare for publication.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use std::{collections::{BTreeMap, HashMap}, fs::File, io::{BufReader, Read, Seek}, path::{Path, PathBuf}};
 
 use anyhow::bail;
