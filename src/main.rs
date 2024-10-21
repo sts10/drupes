79fd876068c138924b7a3b69dc6e7744dbb046a6
Cliff L. Biffle <code@cliffle.com>
2024-10-20 21:34:22 -0700

    Add forgotten import, whoops.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::{collections::{BTreeMap, HashMap}, fs::File, io::{BufReader, ErrorKind, Read, Seek}, path::{Path, PathBuf}};
+use std::{collections::{BTreeMap, HashMap}, fs::File, io::{BufReader, ErrorKind, Read, Seek}, path::{Path, PathBuf}, time::Instant};
 
 use anyhow::bail;
 use clap::Parser;
